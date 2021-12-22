6f5efd54e0a1ed64f168e2840d9c57d80a5edcf4
America M <america-develops@gmail.com>
2021-12-22 14:51:50 -0500

    Create scripts.js file to hold javascript code externally.

---
+++ scripts.js
@@ -0,0 +1,14 @@
+/*  FILE INFORMATION
+    ---------------------
+    Author: America Murray
+    Date created: 12/21/2021
+    Name: scripts.js
+
+    Description: Javascript scripts.
+
+*/
+
+// CONSTANTS
+
+
+// FUNCTIONS
